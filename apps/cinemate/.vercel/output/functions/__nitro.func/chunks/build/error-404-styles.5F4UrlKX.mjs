b22c6ef0c66ba175121c522b741e690ee94b080b
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-11-02 22:52:40 +0800

    fix: remove output file

--- apps/cinemate/.vercel/output/functions/__nitro.func/chunks/build/error-404-styles.5F4UrlKX.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error404_vue_vue_type_style_index_0_scoped_d66eb193_lang } from './error-404-styles-1.mjs-DProOQoh.mjs';
-
-const error404Styles_5F4UrlKX = [error404_vue_vue_type_style_index_0_scoped_d66eb193_lang];
-
-export { error404Styles_5F4UrlKX as default };
-//# sourceMappingURL=error-404-styles.5F4UrlKX.mjs.map
--- apps/cinemate/.vercel/output/functions/__nitro.func/chunks/build/error-500-styles.ndgBtHJ6.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error500_vue_vue_type_style_index_0_scoped_2c336a06_lang } from './error-500-styles-1.mjs-CXn48b9y.mjs';
-
-const error500Styles_ndgBtHJ6 = [error500_vue_vue_type_style_index_0_scoped_2c336a06_lang];
-
-export { error500Styles_ndgBtHJ6 as default };
-//# sourceMappingURL=error-500-styles.ndgBtHJ6.mjs.map
